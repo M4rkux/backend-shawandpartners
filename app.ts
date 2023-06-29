d1f03024ca3160bb58d2ccde0db0b85fd1485407
Marcus Amaral Rolim <m4rkux@gmail.com>
2023-06-28 19:09:47 -0700

    Add cors

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import usersRouter from './routes/users.routes';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
 
 app.use('/api', usersRouter);
 
